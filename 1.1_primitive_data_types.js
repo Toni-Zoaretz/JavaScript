2fc7af032aeedca093180d7d76a8b61f7d1648a1
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-08 11:36:16 +0200

    2.1_functions

--- 1.1_primitive_data_types.js
+++ 1.1_primitive_data_types.js
@@ -1,18 +1,18 @@
-const str = "toni zoaretz";
-const number = 10;
-const obj = {
-  first_name: "toni",
-  last_name: "zoaertz",
-  age: 27,
-  friends: ["noa", "sean", "shely"],
-};
-const isMale = false;
-const result = null;
-const symbol = "!";
+// const str = "toni zoaretz";
+// const number = 10;
+// const obj = {
+//   first_name: "toni",
+//   last_name: "zoaertz",
+//   age: 27,
+//   friends: ["noa", "sean", "shely"],
+// };
+// const isMale = false;
+// const result = null;
+// const symbol = "!";
 
-console.log(typeof str);
-console.log(typeof number);
-console.log(typeof obj);
-console.log(typeof isMale);
-console.log(typeof result);
-console.log(typeof symbol);
+// console.log(typeof str);
+// console.log(typeof number);
+// console.log(typeof obj);
+// console.log(typeof isMale);
+// console.log(typeof result);
+// console.log(typeof symbol);
--- 1.2_more_on_primitive_data_types.js
+++ 1.2_more_on_primitive_data_types.js
@@ -27,27 +27,27 @@
 //create three different ways to declare variables +
 //declare a variable and reassign a value to it:
 
-let ans = "varible";
-const ans2 = "varible2";
-let ans3;
-ans3 = 10 + 2;
+// let ans = "varible";
+// const ans2 = "varible2";
+// let ans3;
+// ans3 = 10 + 2;
 
 //create a variable and after that assign a string with its value being:
 //He's got it!:
-let answer = "He`s got it!";
+// let answer = "He`s got it!";
 
 //create a variable and assign a value on how much a restaurant bill is:
-let bill = 100;
+// let bill = 100;
 
 //create a variable and assign a value on how much tax they should pay:
-let tax = 1.17;
+// let tax = 1.17;
 
 //create a variable that will calculate the bill
 //tax and its output would be: Your total bill is:
-let finalBill = "Your total bill is" + " " + bill * tax;
+// let finalBill = "Your total bill is" + " " + bill * tax;
 
 // Round the number 50.6 to its nearest integer:
-console.log(Math.round(50.6));
+// console.log(Math.round(50.6));
 
 ///Create a variable that is undefined:
-let x;
+// let x;
