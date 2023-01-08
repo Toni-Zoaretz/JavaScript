9e8e35d9d33d2fa3a15352446e37ae23897c460d
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-08 15:12:13 +0200

    2.4_exr

---
+++ 2.4_date.js
@@ -0,0 +1,22 @@
+// Let us self-learn something. You come to your new workplace
+// and you are an expert in the technologies your company is
+// working with. But all of the sudden your boss says. We want to
+// implement a feature with dates. This will happen all the time in
+// your career. So let's roll up our sleeves and start googling. How
+// to …. “date js”.
+// And as always the MDN docs are a great source Find how to
+// get the following:
+// 1. Name of the current day e.g. Sunday
+// 2. Current day of the month e.g. 31
+// 3. Current month e.g January
+// 4. Current Year e.g 2021
+// Create a function that will return a string in the following format:
+
+// `Today is Sunday the 31 of January 2021’
+
+function datePrinter() {
+  const dayOfWeek = new Date().toLocaleDateString("en-US", { weekday: "long" });
+  const dayOfMonth = new Date().getDate();
+  const month = new Date().toLocaleString("default", { month: "long" });
+  const year = new Date().getFullYear();
+}
