359c26fd7a246893a9a07761f405942d2f5308d5
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-08 11:16:27 +0200

    1.1_primitive_data_types

---
+++ 1.2_more_on_primitive_data_types.js
@@ -0,0 +1,53 @@
+// ***Which of the following is/are string?***
+// c) '4'
+
+// ***Which of the following is/are number?***
+// a) 4
+// b) 4.0
+// d)-4
+
+// ***Which of the following is/are booleans?***
+// a) true
+// b) false
+
+// ***What is the result of 80 + 71.2?***
+// a) 151.2
+
+// ***What is the result of "80" + 71.2?***
+// c)"8071.2"
+
+// ***Does 100 + 30 produce a number or a string?***
+// a)number
+
+// ***Does "100" + 30 produce a number or a string?***
+// b)string
+
+//----Question----//
+
+//create three different ways to declare variables +
+//declare a variable and reassign a value to it:
+
+let ans = "varible";
+const ans2 = "varible2";
+let ans3;
+ans3 = 10 + 2;
+
+//create a variable and after that assign a string with its value being:
+//He's got it!:
+let answer = "He`s got it!";
+
+//create a variable and assign a value on how much a restaurant bill is:
+let bill = 100;
+
+//create a variable and assign a value on how much tax they should pay:
+let tax = 1.17;
+
+//create a variable that will calculate the bill
+//tax and its output would be: Your total bill is:
+let finalBill = "Your total bill is" + " " + bill * tax;
+
+// Round the number 50.6 to its nearest integer:
+console.log(Math.round(50.6));
+
+///Create a variable that is undefined:
+let x;
