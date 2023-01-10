87a95c486ddc7590f0b9943d097744e9fccccec8
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-10 12:31:44 +0200

    6.1

---
+++ 6.2_Array Basics.js
@@ -0,0 +1,32 @@
+const people = ["Greg", "Mary", "Devon", "James"];
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i]);
+}
+/////////////////
+people.shift();
+console.log(people);
+////////////////
+people.pop();
+console.log(people);
+////////////////
+people.unshift("Matt");
+console.log(people);
+////////////////
+people.push("Toni");
+console.log(people);
+////////////////
+for (let i = 0; i < people.length; i++) {
+  if (people[i] === "Mary") break;
+  {
+    // console.log(people[i]);
+  }
+}
+////////////////
+// console.log(people.slice(2));
+////////////////
+// console.log(people.indexOf("Mary"));
+////////////////
+// console.log(people.indexOf("foo"));
+////////////////
+const spl = people.splice(1, 1, "Elizabeth", "Artie");
+console.log(spl);
