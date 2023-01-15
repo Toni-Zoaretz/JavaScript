df1fc3eb40048d1f6c310558c5f59469f0aafe5e
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-15 11:44:48 +0200

    9.2

---
+++ 9.2_while_loop.js
@@ -0,0 +1,14 @@
+function sum(arr) {
+  let result = [];
+  let i = 0;
+  while (i < arr.length) {
+    result.push(arr[i].length);
+    i++;
+  }
+  return result;
+}
+
+console.log(sum(["boo", "doooo", "hoo", "ro"]));
+
+// /*******/
+// The for loop i think :)
