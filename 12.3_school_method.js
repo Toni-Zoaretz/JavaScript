509b157b50c2ebc88a237c42fd4079797d38c309
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-16 23:31:00 +0200

    12.3

---
+++ 12.3_school_method.js
@@ -0,0 +1,50 @@
+const school = {
+  teachers: [
+    {
+      id: 1,
+      name: "Pinchas",
+      subjects: ["chemistry", "biology", "physics"],
+      students: [],
+      capacityLeft: 3,
+    },
+    {
+      id: 2,
+      name: "Williams",
+      subjects: ["history", "ethics"],
+      students: [],
+      capacityLeft: 2,
+    },
+  ],
+  students: [
+    {
+      id: 10,
+      name: "Jennifer",
+      age: 20,
+    },
+    {
+      id: 11,
+      name: "Howard",
+      age: 23,
+    },
+    {
+      id: 12,
+      name: "Old-Timmy",
+      age: 86,
+    },
+    {
+      id: 13,
+      name: "Houston",
+      age: 21,
+    },
+  ],
+  findPerson: function (type, id) {
+    return this[type].filter((element) => {
+      return element.id === id;
+    });
+  },
+  assignStudent: function (id, subject) {
+    const availableTeacher = this.teachers.find((teacher) => {
+      return teacher.subjects.includes(subject) && teacher.capacityLeft > 0;
+    })
+
+    
