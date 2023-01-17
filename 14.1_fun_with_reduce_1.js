dbf2256769cc61b6b70e902814febee0704988ab
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-17 15:40:53 +0200

    13.2

---
+++ 14.1_fun_with_reduce_1.js
@@ -0,0 +1,37 @@
+// const arr1 = [10, 2, 8, 13];
+
+// function findMax(arr) {
+//   const result = arr1.reduce((acc, cur) => {
+//     acc = Math.max(cur);
+//     return acc;
+//   });
+
+//   return result;
+// }
+
+// console.log(findMax([10, 2, 8, 13]));
+// // console.log(result);
+
+// function evenNumber(arr) {
+//   const result2 = arr1.reduce((accu, curr) => {
+//     if (curr % 2 === 0) {
+//       accu += curr;
+//     } else {
+//       accu += 0;
+//     }
+//     return accu;
+//   });
+
+//   return result2;
+// }
+
+// console.log(evenNumber(arr1));
+
+const arr1 = [10, 2, 8, 13]; //8.25
+
+const avg = (arr) => {
+  const sum = arr1.reduce((accu, curr) => accu + curr);
+  return sum / arr.length;
+};
+
+console.log(avg(arr1));
