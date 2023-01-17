19ea3b393284de1f1899f720fdb9e4d6ae0280c5
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-17 12:54:15 +0200

    13.1

---
+++ 13.1_sort_numbers.js
@@ -0,0 +1,8 @@
+const numbers1 = [1, -5, 666, 2, 400, 11];
+const numbers2 = [1, -5, 666, 2, 400, 11];
+
+const sort1 = numbers1.sort((a, b) => a - b);
+const sort2 = numbers2.sort((a, b) => b - a);
+
+console.log(sort1);
+console.log(sort2);
---
+++ 13.2_sort_strings.js
@@ -0,0 +1,18 @@
+// const food = ["afalafel", "dsabich", "bhummus", "cpizza with extra pineapple"];
+// const food3 = ["afalafel", "dsabich", "bhummus", "cpizza with extra pineapple"];
+
+// const foods2 = food.sort();
+// console.log(food);
+
+// const food4 = food3.sort().reverse();
+// console.log(food3);
+
+const foodsWithUpperCase = [
+  "falafel",
+  "Sabich",
+  "hummus",
+  "pizza with extra pineapple",
+];
+
+const foodsWithUpperCase1 = foodsWithUpperCase.sort();
+console.log(foodsWithUpperCase1);
