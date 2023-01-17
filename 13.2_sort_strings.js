12c20aa98d8f79ae63e2347115499c1be025ca7b
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-17 13:51:40 +0200

    12.3

--- 13.2_sort_strings.js
+++ 13.2_sort_strings.js
@@ -1,11 +1,11 @@
-// const food = ["afalafel", "dsabich", "bhummus", "cpizza with extra pineapple"];
-// const food3 = ["afalafel", "dsabich", "bhummus", "cpizza with extra pineapple"];
+const food = ["afalafel", "dsabich", "bhummus", "cpizza with extra pineapple"];
+const food3 = ["afalafel", "dsabich", "bhummus", "cpizza with extra pineapple"];
 
-// const foods2 = food.sort();
-// console.log(food);
+const foods2 = food.sort();
+console.log(food);
 
-// const food4 = food3.sort().reverse();
-// console.log(food3);
+const food4 = food3.sort().reverse();
+console.log(food3);
 
 const foodsWithUpperCase = [
   "falafel",
@@ -16,3 +16,43 @@ const foodsWithUpperCase = [
 
 const foodsWithUpperCase1 = foodsWithUpperCase.sort();
 console.log(foodsWithUpperCase1);
+
+foodsWithUpperCase.sort((a, b) => {
+  if (a.toLowerCase() > b.toLowerCase()) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+console.log(foodsWithUpperCase);
+
+foodsWithUpperCase.sort((a, b) => {
+  if (b.toLowerCase() > a.toLowerCase()) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+console.log(foodsWithUpperCase);
+
+const words = ["apple", "supercalifragilisticexpialidocious", "hi", "zoo"];
+
+function counter(words1) {
+  let count = [];
+  words1.forEach(function (a) {
+    count.push(a.length);
+  });
+  return count;
+}
+
+console.log(counter(words));
+
+console.log(counter);
+const wordsSort = words.sort();
+
+console.log(words);
+
+const wordsAcenToDec = words.sort((a, b) => b.length - a.length);
+console.log(wordsAcenToDec);
