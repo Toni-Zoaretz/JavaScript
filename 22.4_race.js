32352e7d10abc208a02ee1a4507690c3237344ab
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-30 20:20:52 +0200

    snake

--- 22.4_race.js
+++ 22.4_race.js
@@ -1,6 +1,6 @@
-"use strict";
+// "use strict";
 
-const player1Race = document.getElementById("player1-race");
-player1Race.style.width = "50" + "px";
-const player2Race = document.getElementById("player2-race");
-player2Race.style.width = "50" + "px";
+// const player1Race = document.getElementById("player1-race");
+// player1Race.style.width = "50" + "px";
+// const player2Race = document.getElementById("player2-race");
+// player2Race.style.width = "50" + "px";
--- 4.1_number_of_siblings.js
+++ 4.1_number_of_siblings.js
@@ -1,9 +1,9 @@
-const numSiblings = prompt("How many siblings do you have?");
-console.log("😈" + typeof numSiblings);
-if (numSiblings === 1) {
-  console.log("1 sibilng");
-} else if (numSiblings > 1) {
-  console.log("'More than 1 sibling");
-} else {
-  console.log("No siblings");
-}
+// const numSiblings = prompt("How many siblings do you have?");
+// console.log("😈" + typeof numSiblings);
+// if (numSiblings === 1) {
+//   console.log("1 sibilng");
+// } else if (numSiblings > 1) {
+//   console.log("'More than 1 sibling");
+// } else {
+//   console.log("No siblings");
+// }
