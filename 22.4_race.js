ad25b75c1cbe7d157b7ccda3b8441aaef1ade2cd
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-01-31 09:01:36 +0200

    22.4

--- 22.4_race.js
+++ 22.4_race.js
@@ -2,30 +2,35 @@
 
 const playe1Race = document.getElementById("player1-race");
 const player2Race = document.getElementById("player2-race");
-let activePlayer = document.querySelector(".active");
-console.log(activePlayer);
+let activePlayer = document.querySelector(".active"); //player1
+// console.log(activePlayer);
 
 window.addEventListener("keyup", function (e) {
   if (e.key === "ArrowRight" || e.key === "Right") {
-    console.log("this is right!!");
   }
 });
 
 window.addEventListener("keyup", function (e) {
   if (e.key === "ArrowLeft" || e.key === "Left") {
-    console.log("this is left");
   }
 });
 
-// console.log(playe1Race);
-
 function td() {
   let active = activePlayer;
+  // let currentMove = "";
+  // console.log(active);
   for (let i = 0, cell; (cell = playe1Race.cells[i]); i++) {
     console.log(cell);
-    // cell.classList.remove(".active");
-    // cell.classList.add(".active");
+    if (!cell.classList.contains("finish")) {
+      active.classList.remove("active");
+      // cell.classList.add("active");
+      cell.classList.add("active");
+    }
+
+    // active = active.classList.remove("active");
+    // active[i + 1].classList.add("active");
   }
+  return active;
 }
 
 td();
