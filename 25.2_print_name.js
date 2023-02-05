fd8b5c55ea86a9ea274394a7806ad9b542b7632f
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-02-05 15:12:37 +0200

    25.2

--- 25.2_print_name.js
+++ 25.2_print_name.js
@@ -2,3 +2,17 @@
 // also have a method that prints its name and another method
 // that prints its name after a second with the help of setTimeOut.
 // in this exercise, you are not allowed to use arrow functions
+
+const obj = {
+  name: "toni",
+
+  print() {
+    console.log(`${this.name}`);
+  },
+
+  callMe() {
+    setTimeout(this.print.bind(this), 1000);
+  },
+};
+
+obj.callMe();
