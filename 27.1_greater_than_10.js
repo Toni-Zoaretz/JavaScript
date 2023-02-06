a86d781ae14500db226d1f4050235675e2caa5e0
Toni-Zoaretz <tonizoaretz1@gmail.com>
2023-02-06 13:58:50 +0200

    27.1

--- 27.1_greater_than_10.js
+++ 27.1_greater_than_10.js
@@ -5,3 +5,38 @@
 // rejected.
 // Call the function and print the resolve and reject in a .then,
 // .catch
+
+// const willGetMeDog = new Promise((resolve, reject) => {
+//   const ten = Math.floor(Math.random() * 20) + 1;
+//   if (ten > 10) {
+//     resolve();
+//   } else {
+//     reject();
+//   }
+// });
+
+// willGetMeDog
+//   .then(() => {
+//     console.log("above 10 baby");
+//   })
+//   .catch(() => {
+//     console.log("below");
+//   })
+
+function greaterThan10(num) {
+  const above10 = new Promise((resolve, reject) => {
+    if (num >= 10) {
+      resolve();
+    } else {
+      reject();
+    }
+  });
+  above10.then(() => {
+    console.log("amazing greater then 10");
+  });
+  above10.catch(() => {
+    console.log("this is below 10");
+  });
+}
+
+greaterThan10(8);
